6c4c860fe12408bafc7513accfe9144147e19610
Mateus Barnabé <poluxz29@gmail.com>
2024-06-19 16:39:21 -0300

    criamos a classe Page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page{
+
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];
+
+    public function __construct($opts = array())
+    {
+        $this->options = array_merge($this->defaults, $opts);
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false
+        );
+
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl;
+
+        $this->setData($this->options["data"]);
+        $this->tpl->draw("header");
+    }
+
+    public function setData($data){
+        foreach($data as $key => $value){
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($name, $data = array(), $retunrHTML = false){
+        $this->setData($data);
+
+        return $this->tpl->draw($name, $retunrHTML);
+    }
+
+    public function __destruct()
+    {
+        $this->tpl->draw("footer");
+    }
+
+}
---
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Hello esse é o meu primeiro código oficial</h1>
